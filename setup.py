5b8dfbefdf4e1307dbc3ea5776fffee0dd56ba01
jcchin <jchin89@gmail.com>
2020-09-28 12:54:44 -0400

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='boring_battery',
       version='1.0.0',
-      packages=find_packages(include=['src', 'example1']),
+      packages=find_packages(),
       py_modules=['example1'],
       install_requires=[
         'openmdao>=2.0.0',
