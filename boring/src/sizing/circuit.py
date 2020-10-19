1a86a55878f13ea6c3312d3b19510d8db1c5e29f
jcchin <jchin89@gmail.com>
2020-10-19 17:37:07 -0400

    debugging with a single resistor

--- boring/src/sizing/circuit.py
+++ boring/src/sizing/circuit.py
@@ -68,60 +68,68 @@ class Circuit(om.Group):
     """ Thermal equivalent circuit """
     def setup(self):
 
-        self.add_subsystem('n1', Node(n_in=1, n_out=2), promotes_inputs=[('q_in:0', 'q_in')])  # 2 out
-        self.add_subsystem('n2', Node(n_in=1, n_out=2))  # 2 out
-        self.add_subsystem('n3', Node(n_in=1, n_out=1))  # 1
-        self.add_subsystem('n4', Node(n_in=1, n_out=1))  # 1
-        self.add_subsystem('n5', Node(n_in=1, n_out=1))  # 1
-        self.add_subsystem('n6', Node(n_in=1, n_out=1))  # 1
-        self.add_subsystem('n7', Node(n_in=2, n_out=1))  # 2 in
-        self.add_subsystem('n8', Node(n_in=2, n_out=1), promotes_inputs=[('q_out:0', 'q_out')])  # 2 in
+        self.add_subsystem('n1', Node(n_in=1, n_out=1))#, promotes_inputs=[('q_in:0', 'q_in')])  # 2 out
+        self.add_subsystem('n2', Node(n_in=1, n_out=1))  # 2 out
+        # self.add_subsystem('n3', Node(n_in=1, n_out=1))  # 1
+        # self.add_subsystem('n4', Node(n_in=1, n_out=1))  # 1
+        # self.add_subsystem('n5', Node(n_in=1, n_out=1))  # 1
+        # self.add_subsystem('n6', Node(n_in=1, n_out=1))  # 1
+        # self.add_subsystem('n7', Node(n_in=2, n_out=1))  # 2 in
+        # self.add_subsystem('n8', Node(n_in=2, n_out=1))#, promotes_inputs=[('q_out:0', 'q_out')])  # 2 in
 
 
-        self.add_subsystem('Rwe', Resistor(), promotes_inputs=[('T_in', 'T_hot')]) # evaporator wall
-        self.add_subsystem('Rwke', Resistor()) # evaporator wick
-        self.add_subsystem('Rinter_e', Resistor())
-        self.add_subsystem('Rv', Resistor()) # vapor
-        self.add_subsystem('Rwka', Resistor()) # wick adiabatic
-        self.add_subsystem('Rwa', Resistor()) # wall adiabatic
-        self.add_subsystem('Rinter_c', Resistor()) #
-        self.add_subsystem('Rwkc', Resistor()) # condensor wick
-        self.add_subsystem('Rwc', Resistor(), promotes_inputs=[('T_out', 'T_cold')]) #condensor wall
+        self.add_subsystem('Rwe', Resistor())#, promotes_inputs=[('T_in', 'T_hot')]) # evaporator wall
+        # self.add_subsystem('Rwke', Resistor()) # evaporator wick
+        # self.add_subsystem('Rinter_e', Resistor())
+        # self.add_subsystem('Rv', Resistor()) # vapor
+        # self.add_subsystem('Rwka', Resistor()) # wick adiabatic
+        # self.add_subsystem('Rwa', Resistor()) # wall adiabatic
+        # self.add_subsystem('Rinter_c', Resistor()) #
+        # self.add_subsystem('Rwkc', Resistor()) # condensor wick
+        # self.add_subsystem('Rwc', Resistor())#, promotes_inputs=[('T_out', 'T_cold')]) #condensor wall
 
         # node 1 (q_in promoted, 'Rwe.T_in',  3 additional connections)
-        self.connect('n1.T', ['Rwa.T_in']) # define temperature node as resitor inputs
+        self.connect('n1.T', ['Rwe.T_in']) # define temperature node as resitor inputs
         self.connect('Rwe.q', 'n1.q_out:0') # connect resistor flux to each node port
-        self.connect('Rwa.q', 'n1.q_out:1')
         # node 2 (6 connections)
         self.connect('Rwe.q', 'n2.q_in:0')
-        self.connect('n2.T', ['Rwke.T_in', 'Rwka.T_in','Rwe.T_out'])
-        self.connect('Rwke.q', 'n2.q_out:0') 
-        self.connect('Rwka.q', 'n2.q_out:1')
-        # node 3 (4 connections)
-        self.connect('Rwke.q', 'n3.q_in:0')
-        self.connect('n3.T', ['Rinter_e.T_in','Rwke.T_out'])
-        self.connect('Rinter_e.q', 'n3.q_out:0')
-        # node 4 (4 connections)
-        self.connect('Rinter_e.q', 'n4.q_in:0')
-        self.connect('n4.T', ['Rv.T_in','Rinter_e.T_out'])
-        self.connect('Rv.q', 'n4.q_out:0')
-        # node 5 (4 connections)
-        self.connect('Rv.q', 'n5.q_in:0')
-        self.connect('n5.T', ['Rinter_c.T_in','Rv.T_out'])
-        self.connect('Rinter_c.q', 'n5.q_out:0')
-        # node 6 (4 connections)
-        self.connect('Rinter_c.q', 'n6.q_in:0')
-        self.connect('n6.T', ['Rwkc.T_in','Rinter_c.T_out'])
-        self.connect('Rwkc.q', 'n6.q_out:0')
-        # node 7 (6 connections)
-        self.connect('Rwka.q', 'n7.q_in:0')
-        self.connect('Rwkc.q', 'n7.q_in:1')
-        self.connect('n7.T', ['Rwc.T_in','Rwkc.T_out','Rwka.T_out'])
-        self.connect('Rwc.q', 'n7.q_out:0')
-        # node 8 (q_out promoted, 'Rwc.T_out', 3 additional connections)
-        self.connect('Rwc.q','n8.q_in:0')
-        self.connect('Rwa.q','n8.q_in:1')
-        self.connect('n8.T',['Rwa.T_out'])
+        self.connect('n2.T', ['Rwe.T_out'])
+
+
+        # # node 1 (q_in promoted, 'Rwe.T_in',  3 additional connections)
+        # self.connect('n1.T', ['Rwa.T_in','Rwe.T_in']) # define temperature node as resitor inputs
+        # self.connect('Rwe.q', 'n1.q_out:0') # connect resistor flux to each node port
+        # self.connect('Rwa.q', 'n1.q_out:1')
+        # # node 2 (6 connections)
+        # self.connect('Rwe.q', 'n2.q_in:0')
+        # self.connect('n2.T', ['Rwke.T_in', 'Rwka.T_in','Rwe.T_out'])
+        # self.connect('Rwke.q', 'n2.q_out:0') 
+        # self.connect('Rwka.q', 'n2.q_out:1')
+        # # node 3 (4 connections)
+        # self.connect('Rwke.q', 'n3.q_in:0')
+        # self.connect('n3.T', ['Rinter_e.T_in','Rwke.T_out'])
+        # self.connect('Rinter_e.q', 'n3.q_out:0')
+        # # node 4 (4 connections)
+        # self.connect('Rinter_e.q', 'n4.q_in:0')
+        # self.connect('n4.T', ['Rv.T_in','Rinter_e.T_out'])
+        # self.connect('Rv.q', 'n4.q_out:0')
+        # # node 5 (4 connections)
+        # self.connect('Rv.q', 'n5.q_in:0')
+        # self.connect('n5.T', ['Rinter_c.T_in','Rv.T_out'])
+        # self.connect('Rinter_c.q', 'n5.q_out:0')
+        # # node 6 (4 connections)
+        # self.connect('Rinter_c.q', 'n6.q_in:0')
+        # self.connect('n6.T', ['Rwkc.T_in','Rinter_c.T_out'])
+        # self.connect('Rwkc.q', 'n6.q_out:0')
+        # # node 7 (6 connections)
+        # self.connect('Rwka.q', 'n7.q_in:0')
+        # self.connect('Rwkc.q', 'n7.q_in:1')
+        # self.connect('n7.T', ['Rwc.T_in','Rwkc.T_out','Rwka.T_out'])
+        # self.connect('Rwc.q', 'n7.q_out:0')
+        # # node 8 (q_out promoted, 'Rwc.T_out', 3 additional connections)
+        # self.connect('Rwc.q','n8.q_in:0')
+        # self.connect('Rwa.q','n8.q_in:1')
+        # self.connect('n8.T',['Rwa.T_out','Rwc.T_out'])
 
 
         self.nonlinear_solver = om.NewtonSolver(solve_subsystems=True)
@@ -137,19 +145,21 @@ if __name__ == "__main__":
     p = om.Problem()
     model = p.model
 
-    model.add_subsystem('T_hot', om.IndepVarComp('T', 500., units='K'))
-    model.add_subsystem('T_cold', om.IndepVarComp('T', 60, units='K'))
+    # model.add_subsystem('T_hot', om.IndepVarComp('T', 500., units='K'))
+    # model.add_subsystem('T_cold', om.IndepVarComp('T', 60, units='K'))
     model.add_subsystem('q_hot', om.IndepVarComp('q', 70., units='W'))
     model.add_subsystem('q_cold', om.IndepVarComp('q', 50, units='W'))
     model.add_subsystem('circuit', Circuit())
 
-    model.connect('T_hot.T', 'circuit.T_hot')
-    model.connect('T_cold.T', 'circuit.T_cold')
-    model.connect('q_hot.q', 'circuit.q_in')
-    model.connect('q_cold.q', 'circuit.q_out')
+    # model.connect('T_hot.T', 'circuit.n1.T')
+    # model.connect('T_cold.T', 'circuit.n2.T')
+    # model.connect('q_hot.q', 'circuit.n1.q_in:0')
+    # model.connect('q_cold.q', 'circuit.n2.q_out:0')
 
     p.setup()
 
+    p.set_val('circuit.Rwe.T_out', 60.)
+    #p.set_val('circuit.n1.q_in:0', 40.)
     print('hello')
     #p.check_partials(compact_print=True)
     #om.n2(p)
@@ -157,11 +167,11 @@ if __name__ == "__main__":
     # set some initial guesses
     p['circuit.n1.T'] = 500.
     p['circuit.n2.T'] = 350.
-    p['circuit.n3.T'] = 300.
-    p['circuit.n4.T'] = 250.
-    p['circuit.n5.T'] = 200.
-    p['circuit.n6.T'] = 150.
-    p['circuit.n7.T'] = 100.
-    p['circuit.n8.T'] = 60.
+    # p['circuit.n3.T'] = 300.
+    # p['circuit.n4.T'] = 250.
+    # p['circuit.n5.T'] = 200.
+    # p['circuit.n6.T'] = 150.
+    # p['circuit.n7.T'] = 100.
+    # p['circuit.n8.T'] = 60.
 
     p.run_model()        
