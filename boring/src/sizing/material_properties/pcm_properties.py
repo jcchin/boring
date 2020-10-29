9bdd23848ee7cc65a5b7d436867937410ff30a8c
jcchin <jchin89@gmail.com>
2020-10-29 15:32:46 -0400

    adding pcm derivatives

--- boring/src/sizing/material_properties/pcm_properties.py
+++ boring/src/sizing/material_properties/pcm_properties.py
@@ -25,15 +25,15 @@ class PCM_props(om.ExplicitComponent):
         self.add_input('porosity', 0.9,  desc='percentage porosity, 1 = completely void, 0 = solid')
         self.add_input('pad_area', 0.0571*.102, units='m', desc='cell frontal area')
         # conductive foam properties
-        self.add_input('k_foam', 1., desc='thermal conductivity of the foam')
-        self.add_input('rho_foam', 1., desc='intrinsic density of the foam material (unrelated to porosity)')
+        self.add_input('k_foam', 401., units='W/m*K', desc='thermal conductivity of the foam')
+        self.add_input('rho_foam', 8960., units='kg/m**3', desc='intrinsic density of the foam material (unrelated to porosity)')
         self.add_input('lh_foam', 0., desc='latent heat of the foam skeleton')
-        self.add_input('cp_foam', 1., desc='specific heat of the foam')
+        self.add_input('cp_foam', 0.39, units='kJ/kg*K', desc='specific heat of the foam')
         # phase change material properties
-        self.add_input('k_pcm', 1., desc='thermal conductivity of the pcm')
-        self.add_input('rho_pcm', 1., desc='intrinsic density of the pcm (unrelated to porosity)')
-        self.add_input('lh_pcm', 1., desc='latent heat of the pcm')
-        self.add_input('cp_pcm', 1., desc='specific heat of the pcm')
+        self.add_input('k_pcm', 2., units='W/m*K', desc='thermal conductivity of the pcm')
+        self.add_input('rho_pcm', 1450., units='kg/m**3', desc='intrinsic density of the pcm (unrelated to porosity)')
+        self.add_input('lh_pcm', 271., units='kJ/kg', desc='latent heat of the pcm')
+        self.add_input('cp_pcm', 1.54, units='kJ/kg*K', desc='specific heat of the pcm')
         # outputs
         self.add_output('k_bulk', units='W/m*K', desc='PCM pad thermal conductivity')
         self.add_output('R_PCM', units='K/W', desc='PCM pad thermal resistance')
@@ -42,7 +42,7 @@ class PCM_props(om.ExplicitComponent):
 
 
     def setup_partials(self):
-        self.declare_partials('*', '*', method='cs')
+        self.declare_partials('*', '*')#, method='cs')
 
 
     def compute(self, inputs, outputs):
@@ -55,6 +55,38 @@ class PCM_props(om.ExplicitComponent):
         outputs['R_PCM'] = inputs['t_pad']/(inputs['pad_area']*outputs['k_bulk'])
 
 
-    # def compute_partials(self, inputs, J):
+    def compute_partials(self, inputs, J):
 
         # add partial derivatives here
+        porosity = inputs['porosity']
+        k_foam = inputs['k_foam']
+        k_pcm = inputs['k_pcm']
+        A = inputs['pad_area']
+        t_pad = inputs['t_pad']
+        k_bulk = 1./(porosity/k_pcm + (1-porosity)/k_foam)
+
+        J['k_bulk','porosity'] = -k_foam*k_pcm*(k_foam-k_pcm)/(k_pcm*(porosity-1.)-k_foam*porosity)**2
+        J['k_bulk','k_pcm'] = k_foam**2*porosity/(k_pcm*(porosity-1.)-k_foam*porosity)**2
+        J['k_bulk','k_foam'] = -k_pcm**2*(porosity-1.)/(k_pcm*(porosity-1.)-k_foam*porosity)**2
+
+
+        J['R_PCM','porosity'] = t_pad*(k_foam-k_pcm)/(A*k_foam*k_pcm)
+        J['R_PCM','k_pcm'] = -porosity*t_pad/(A*k_pcm**2)
+        J['R_PCM','k_foam'] = (porosity-1)*t_pad/(A*k_foam**2)
+        J['R_PCM','t_pad'] = 1./(inputs['pad_area']*k_bulk)
+        J['R_PCM','pad_area'] = -t_pad/(k_bulk*inputs['pad_area']**2)
+
+if __name__ == '__main__':
+    from openmdao.api import Problem
+
+    nn = 1
+
+    prob = Problem()
+    prob.model.add_subsystem('comp1', PCM_props(num_nodes=nn), promotes_outputs=['*'], promotes_inputs=['*'])
+    prob.setup(force_alloc_complex=True)
+    prob.run_model()
+    prob.check_partials(method='cs', compact_print=True)
+
+
+
+
