091cde83d3f2422a527bccb7a534ffe606b6e5af
dlhall04 <dustin.hall04@gmail.com>
2020-12-18 20:04:35 -0500

    meta_group updated to include the meta temperature profiles. Sizing component is not based on the diagonal measurements, and the var extra is properly accounted for. Training_data was made into a group. Viewer_data includes all dimensions.

--- boring/metamodel/sizing_component.py
+++ boring/metamodel/sizing_component.py
@@ -3,7 +3,7 @@ An example battery pack used for reference, based on COMSOL data. The purpose
 of this model is to size a battery pack, and check power input and heat outputs
 against the COMSOL model.
 
-Author: Dustin Hall
+Author: Dustin Hall, Jeff Chin
 """
 import openmdao.api as om
 import numpy as np
@@ -20,23 +20,27 @@ class MetaPackSizeComp(om.ExplicitComponent):
 
         self.add_input('cell_rad', 9, units='mm', desc='radius of an 18650 cell')
         self.add_input('extra',  1, units='mm', desc='extra spacing along the diagonal')
-        self.add_input('ratio', 1.1, desc='cell radius to cutout radius')
+        self.add_input('ratio', 1., desc='cell radius to cutout radius')
         self.add_input('length', 65, units='mm', desc='length of the case')
         self.add_input('al_density', 2700, units='kg/m**3', desc='density of aluminum')
         self.add_input('n', 4 ,  desc='cell array deminsion')
 
-        self.add_output('area', 1000, units='mm**2', desc='final area of the surface')
+        self.add_output('diagonal', 100, units='mm', desc='diagonal length of the square case')
+        self.add_output('side', 100, units='mm', desc='side length of the case')
         self.add_output('solid_area', 1000, units='mm**2', desc='solid surface area assuming no cutouts')
         self.add_output('cell_cutout_area', 800, units='mm**2', desc='area removed for cells')
         self.add_output('air_cutout_area', 800, units='mm**2', desc='area removed for air')
+        self.add_output('area', 1000, units='mm**2', desc='final area of the surface')
         self.add_output('volume', 85000, units='mm**3', desc='volume of the case with voids removed')
         self.add_output('mass', 5, units='kg', desc='mass of the case')
 
         r = c = np.arange(nn) 
-        self.declare_partials('area', ['cell_rad', 'extra', 'n', 'ratio'])
-        self.declare_partials('solid_area', ['cell_rad', 'extra', 'n'])
+        self.declare_partials('diagonal', ['n', 'cell_rad', 'ratio', 'extra'])
+        self.declare_partials('side', ['n', 'cell_rad', 'ratio', 'extra'])
+        self.declare_partials('solid_area', ['n', 'cell_rad', 'ratio', 'extra'])
         self.declare_partials('cell_cutout_area', ['cell_rad', 'n'])
         self.declare_partials('air_cutout_area', ['cell_rad', 'ratio', 'n'])
+        self.declare_partials('area', ['cell_rad', 'extra', 'n', 'ratio'])
         self.declare_partials('volume', ['cell_rad', 'extra', 'n', 'ratio', 'length'])
         self.declare_partials('mass', ['cell_rad', 'extra', 'n', 'ratio', 'length', 'al_density'])
       
@@ -49,16 +53,13 @@ class MetaPackSizeComp(om.ExplicitComponent):
         ratio = inputs['ratio']
         al_density = inputs['al_density']
         
-        '''
-        area = (square with sides equal to the diameter + the extra) - (cell cutouts) - (air cutouts)
-        '''
-        diagonal = (n*cell_d)+((n)*(cell_d/ratio))*extra    ## From Jeff
-        side = diagonal/(2^0.5)                             ## From Jeff
-        solid_area = side^2                                 ## From Jeff
+        outputs['diagonal'] = (n*cell_rad*2)+((n)*(cell_rad*2/ratio))*extra
+        outputs['side'] = outputs['diagonal']/(2**0.5)
+        outputs['solid_area'] = outputs['side']**2
 
-        outputs['solid_area'] = ( (2*cell_rad + 2*extra)**2) **2 
         outputs['cell_cutout_area'] = (pi*cell_rad**2)*n**2
-        outputs['air_cutout_area'] = (pi*(cell_rad*ratio)**2)*n**2
+        outputs['air_cutout_area'] = (pi*(cell_rad/ratio)**2)*n**2
+
         outputs['area'] = outputs['solid_area'] - outputs['cell_cutout_area'] - outputs['air_cutout_area']
 
         outputs['volume'] = outputs['area'] * length
@@ -74,34 +75,70 @@ class MetaPackSizeComp(om.ExplicitComponent):
         ratio = inputs['ratio']
         al_density = inputs['al_density']
 
-        J['area', 'cell_rad'] = 4*n**2*(2*cell_rad + 2*extra) - 2*pi*cell_rad*n**2 - 2*pi*cell_rad*ratio**2 * n**2
-        J['area', 'extra']    = 4*n**2*(2*cell_rad + 2*extra) 
-        J['area', 'n']        = ( (2*cell_rad + 2*extra)**2) * n*2 - (pi*cell_rad**2)*n*2 - (pi*(cell_rad*ratio)**2)*n*2
-        J['area', 'ratio']    = -2*pi*n**2*cell_rad**2 * ratio
+        diagonal = (n*cell_rad*2)+((n)*(cell_rad*2/ratio))*extra
+        side = diagonal/(2**0.5)
+        solid_area = side**2
+        cell_cutout_area = (pi*cell_rad**2)*n**2
+        air_cutout_area = (pi*(cell_rad/ratio)**2)*n**2
 
-        J['solid_area', 'cell_rad'] = 4*n**2*(2*cell_rad + 2*extra)
-        J['solid_area', 'extra'] = 4*n**2*(2*cell_rad + 2*extra) 
-        J['solid_area', 'n'] = ( (2*cell_rad + 2*extra)**2) * n*2
+        area = solid_area - cell_cutout_area - air_cutout_area
+
+        d_diagonal__d_n         = 2*cell_rad*(1+extra/ratio)
+        d_diagonal__d_cell_rad  = 2*n*(1+extra/ratio)
+        d_diagonal__d_ratio     = -2*cell_rad*n*(extra/ratio**2)
+        d_diagonal__d_extra     = 2*cell_rad*n*(1/ratio)
+
+        d_side__d_n       =  d_diagonal__d_n       /2**0.5
+        d_side__d_cell_rad=  d_diagonal__d_cell_rad/2**0.5
+        d_side__d_ratio   =  d_diagonal__d_ratio   /2**0.5
+        d_side__d_extra   =  d_diagonal__d_extra   /2**0.5
+
+        d_solid_area__d_n        = 2*(side)*(d_side__d_n)
+        d_solid_area__d_cell_rad = 2*(side)*(d_side__d_cell_rad)
+        d_solid_area__d_ratio    = 2*(side)*(d_side__d_ratio)
+        d_solid_area__d_extra    = 2*(side)*(d_side__d_extra)
+        
+
+        J['diagonal', 'n']          =  d_diagonal__d_n       
+        J['diagonal', 'cell_rad']   =  d_diagonal__d_cell_rad
+        J['diagonal', 'ratio']      =  d_diagonal__d_ratio   
+        J['diagonal', 'extra']      =  d_diagonal__d_extra   
+
+        J['side', 'n']          = d_side__d_n       
+        J['side', 'cell_rad']   = d_side__d_cell_rad
+        J['side', 'ratio']      = d_side__d_ratio   
+        J['side', 'extra']      = d_side__d_extra   
+
+
+        J['solid_area', 'n']        = d_solid_area__d_n       
+        J['solid_area', 'cell_rad'] = d_solid_area__d_cell_rad
+        J['solid_area', 'ratio']    = d_solid_area__d_ratio   
+        J['solid_area', 'extra']    = d_solid_area__d_extra   
 
         J['cell_cutout_area', 'cell_rad'] = 2*pi*cell_rad*n**2
         J['cell_cutout_area', 'n'] = (pi*cell_rad**2)*n*2
 
-        J['air_cutout_area', 'cell_rad'] = 2*pi*cell_rad*ratio**2 * n**2
-        J['air_cutout_area', 'ratio'] = 2*pi*n**2*cell_rad**2 * ratio
-        J['air_cutout_area', 'n'] = (pi*(cell_rad*ratio)**2)*n*2
+        J['air_cutout_area', 'cell_rad'] = 2*pi*cell_rad/ratio**2 * n**2
+        J['air_cutout_area', 'ratio'] = -2*pi*n**2*cell_rad**2 / ratio**3
+        J['air_cutout_area', 'n'] = (pi*(cell_rad/ratio)**2)*n*2
 
-        J['volume', 'cell_rad'] = (4*n**2*(2*cell_rad + 2*extra) - 2*pi*cell_rad*n**2 - 2*pi*cell_rad*ratio**2 * n**2) * length
-        J['volume', 'extra']    = 4*n**2*(2*cell_rad + 2*extra)  * length
-        J['volume', 'n']        = (( (2*cell_rad + 2*extra)**2) * n*2 - (pi*cell_rad**2)*n*2 - (pi*(cell_rad*ratio)**2)*n*2 )* length
-        J['volume', 'ratio']    = -2*pi*n**2*cell_rad**2 * ratio * length
-        J['volume', 'length']   = (( (2*cell_rad + 2*extra)**2) * n**2 - (pi*cell_rad**2)*n**2 - (pi*(cell_rad*ratio)**2)*n**2 )
+        J['area', 'cell_rad'] = d_solid_area__d_cell_rad - 2*pi*cell_rad*n**2 - 2*pi*cell_rad/ratio**2 * n**2
+        J['area', 'extra']    = d_solid_area__d_extra
+        J['area', 'n']        = d_solid_area__d_n - (pi*cell_rad**2)*n*2 - (pi*(cell_rad/ratio)**2)*n*2
+        J['area', 'ratio']    = d_solid_area__d_ratio + 2*pi*n**2*cell_rad**2 / ratio**3
 
-        J['mass', 'cell_rad'] = ( 4*n**2*(2*cell_rad + 2*extra) - 2*pi*cell_rad*n**2 - 2*pi*cell_rad*ratio**2 * n**2 ) * length * al_density
-        J['mass', 'extra']    = (4*n**2*(2*cell_rad + 2*extra)  ) * length * al_density
-        J['mass', 'n']        = ( ( (2*cell_rad + 2*extra)**2) * n*2 - (pi*cell_rad**2)*n*2 - (pi*(cell_rad*ratio)**2)*n*2 ) * length * al_density
-        J['mass', 'ratio']    = -2*pi*n**2*cell_rad**2 * ratio * length * al_density
-        J['mass', 'length']   = (( (2*cell_rad + 2*extra)**2) * n**2 - (pi*cell_rad**2)*n**2 - (pi*(cell_rad*ratio)**2)*n**2 )* al_density
-        J['mass', 'al_density']  = (( (2*cell_rad + 2*extra)**2) * n**2 - (pi*cell_rad**2)*n**2 - (pi*(cell_rad*ratio)**2)*n**2) * length
+        J['volume', 'cell_rad'] = (d_solid_area__d_cell_rad - 2*pi*cell_rad*n**2 - 2*pi*cell_rad/ratio**2 * n**2) * length
+        J['volume', 'extra']    = (d_solid_area__d_extra) * length 
+        J['volume', 'n']        = (d_solid_area__d_n - (pi*cell_rad**2)*n*2 - (pi*(cell_rad/ratio)**2)*n*2) * length 
+        J['volume', 'ratio']    = (d_solid_area__d_ratio + 2*pi*n**2*cell_rad**2 / ratio**3) * length 
+        J['volume', 'length']   = area 
+
+        J['mass', 'cell_rad']   = (d_solid_area__d_cell_rad - 2*pi*cell_rad*n**2 - 2*pi*cell_rad/ratio**2 * n**2) * length*al_density
+        J['mass', 'extra']      = (d_solid_area__d_extra) * length * al_density
+        J['mass', 'n']          = (d_solid_area__d_n - (pi*cell_rad**2)*n*2 - (pi*(cell_rad/ratio)**2)*n*2) * length * al_density
+        J['mass', 'ratio']      = (d_solid_area__d_ratio - -2*pi*n**2*cell_rad**2 / ratio**3) * length * al_density
+        J['mass', 'length']     = area * al_density
+        J['mass', 'al_density'] = area * length
 
 
 
